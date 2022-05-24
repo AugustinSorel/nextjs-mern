2676e5e71a8bc6267d5a4c9e43a47ce4cd021dbe
AugustinSorel <sorelaugustin@gmail.com>
2022-05-24 19:05:35 +0200

    test

--- client/pages/add-new-contact/index.tsx
+++ client/pages/add-new-contact/index.tsx
@@ -10,7 +10,7 @@ function AddNewContactPage() {
         <meta name="description" content="add a new contact" />
         <link rel="icon" href="/favicon.ico" />
         <meta
-          http-equiv="Content-Security-Policy"
+          httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         />
       </Head>
