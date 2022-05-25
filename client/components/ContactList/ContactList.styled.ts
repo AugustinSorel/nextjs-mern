94a95616c5d5ab210a22599b0beb73044c43cd02
AugustinSorel <sorelaugustin@gmail.com>
2022-05-25 19:33:55 +0200

    cleaner code

--- client/components/ContactList/ContactList.styled.ts
+++ client/components/ContactList/ContactList.styled.ts
@@ -1,9 +1,15 @@
 import { motion } from "framer-motion";
 import styled, { css } from "styled-components";
+import devices from "../../styles/devices";
 
 export const List = styled(motion.ul)`
   max-width: 75%;
   margin: auto;
+
+  @media ${devices.mobile} {
+    max-width: 100%;
+    padding: var(--gap);
+  }
 `;
 
 export const ListItem = styled(motion.li)`
